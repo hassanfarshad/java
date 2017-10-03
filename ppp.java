d03fac6e187fd498f645e7f34909ec1b261cbaa1
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2017-10-03 16:11:14 +0530

    Create ppp.java

---
+++ ppp.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class Power 
+{
+public static void main(String[] args) 
+{
+int a = 3,b = 4;
+long result = 1;
+while (b != 0)
+{
+result *= a;
+--b;
+}
+System.out.println("Answer = " + result);
+}
+}
