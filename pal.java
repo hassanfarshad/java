0160071972a7b741ebd93434c6086e2e06e8720c
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-01-08 16:30:03 +0530

    Update pal.java

--- pal.java
+++ pal.java
@@ -9,7 +9,7 @@ Scanner in = new Scanner(System.in);
 System.out.println("Enter a integer to check if it is a palindrome");
 a = in.nextLine();
 int length = a.length();
-for ( int i = length - 1; i >= 0; i-- )
+for ( int i = length - 1; i<=1000; i-- )
 b = b + a.charAt(i);
 if (a.equals(b))
 System.out.println("Entered integer is a palindrome.");
