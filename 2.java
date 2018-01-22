bef9ddad57002fa413458c96ebde52562eca5dbf
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-01-22 16:53:56 +0530

    Update 2.java

--- 2.java
+++ 2.java
@@ -1,6 +1,20 @@
 import java.io.*;
+import java.util.Scanner;
 public class ex2
 {
 public static void main(String[] args)
 {
 int n;
+Scanner s = new Scanner(System.in);
+System.out.print("Enter the number:\n");
+n = s.nextInt();
+  if(n%2==0)
+  {
+    System.out.println("the given number is even");
+  }
+  else
+    {
+    System.out.println("the given number is odd");
+  }
+}
+}
