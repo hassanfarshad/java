2739a382fe3378dcd65f4fbf1a821472516a980b
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-01-22 16:45:55 +0530

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.io.*;
+public class ex1
+{
+public static void main(String[] args) 
+{
+int n;
+Scanner s = new Scanner(System.in);
+System.out.print("Enter the number:\n");
+n = s.nextInt();
+if(n > 0)
+{
+System.out.println("The given number is Positive");
+}
+else
+{
+System.out.println("The given number is Negative");
+}
+}
+}
