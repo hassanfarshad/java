faddb0e6a2bcf7c42494fb3c31edb1851abce804
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-01-22 16:31:10 +0530

    Create dddddd.java

---
+++ dddddd.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class dddddd
+{
+
+public static void main(String args[])
+
+{
+
+int a=5,b;
+for(int i=0;i<=0;i++)
+
+{
+
+ b=a*a;
+
+}
+
+System.out.println("output is",+a);
+
+}
+
+}
