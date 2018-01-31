ae3edcabb2b794d01a16cfa22441d587428844b5
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-01-31 16:35:14 +0530

    Create e22.java

---
+++ e22.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+class e22
+{
+public static void main(String args[])
+{
+Scanner i=new Scanner(System.in);
+System.out.println("enter number a:");
+int a= i.nextInt();
+System.out.println("enter number b:");
+int b= i.nextInt();
+System.out.println("enter number c:");
+int c= i.nextInt();
+if(a>b&&a>c)
+{
+System.out.println("A is greater:");	
+}
+else if(b>c)
+{
+System.out.println("B is greater");
+}
+else
+{
+System.out.println("c is greater");
+}
+}
+}
