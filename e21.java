14cb7f2bff9326460fb9a7177ad145e27d1c8c96
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-01-31 16:11:34 +0530

    Create e21.java

---
+++ e21.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class e21
+{
+public static void main(String args[])
+{
+int sum;
+Scanner i=new Scanner(System.in);
+System.out.println("enter number n:");
+int n= i.nextInt();
+System.out.println("enter number d:");
+int d= i.nextInt();
+System.out.println("enter number a:");
+int a= i.nextInt();
+sum =n*(d+a);
+System.out.println("Result is:"+sum);
+}
+}
