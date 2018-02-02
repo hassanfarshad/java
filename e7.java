51738cc42bb6aa8c4829c93fbf1aa764bb9c5ac7
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-02-02 16:48:04 +0530

    Create e7.java

---
+++ e7.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class JavaExample
+{
+public static void main(String[] arg)
+{
+Scanner d1=new Scanner(System.in);
+System.out.println("Enter the one YEAR : ");
+int a=d1.nextInt(); 
+for(int i=0;i<=a;i++)
+{
+	System.out.println("HELLO");
+}
+}
+}
+
