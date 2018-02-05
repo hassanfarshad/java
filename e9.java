976b8f49631f2b2635f5795afed5a8fdb06aee21
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-02-05 16:45:34 +0530

    Create e9.java

---
+++ e9.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+class e9
+{
+	public static void main(String args[])
+	{
+		int[] aa = {1,2,3,4,5,6,7,8,9};
+		Scanner d1=new Scanner(System.in);
+		System.out.println("Enter the two numbers");
+		int a=d1.nextInt();
+		int b=d1.nextInt();
+		int add=aa[0]+aa[1];
+		System.out.println("OUTPUT IS"+add);
+		}
+}
