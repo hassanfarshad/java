3bf28b1372ea1ff421928236958c069a57bbb1d7
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-02-05 16:31:03 +0530

    Create e11j.java

---
+++ e11j.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class e11 
+{
+public static void main(String[] args) 
+{
+int a1 = 3,b1 = 4;
+long r = 1;
+while (b1 != 0)
+{
+result *= a1;
+--b1;
+}
+System.out.println("Answer = " + result);
+}
+}
