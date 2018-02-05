beaf0e8e112d69ea558172cad605bfd6a5c67fb1
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-02-05 16:33:43 +0530

    Update e12j.java

--- e12j.java
+++ e12j.java
@@ -4,14 +4,14 @@ class Pal
 {
 public static void main(String args[])
 {
-int a, b = "";
+int a1, b1 = "";
 Scanner in = new Scanner(System.in);
 System.out.println("Enter a integer to check if it is a palindrome");
 a = in.nextLine();
-int length = a.length();
+int length = a1.length();
 for ( int i = length - 1; i<=1000; i-- )
-b = b + a.charAt(i);
-if (a.equals(b))
+b1 = b1 + a1.charAt(i);
+if (a1.equals(b1))
 System.out.println("Entered integer is a palindrome.");
 else
 System.out.println("Entered integer is not a palindrome.");
