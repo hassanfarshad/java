4912041b407519d68f7de66aa0219ec206ca6132
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-02-05 16:30:03 +0530

    Update e11.java

--- e11.java
+++ e11.java
@@ -3,12 +3,12 @@ public class e11
 {
 public static void main(String[] args) 
 {
-int a = 3,b = 4;
-long result = 1;
-while (b != 0)
+int a1 = 3,b1 = 4;
+long r = 1;
+while (b1 != 0)
 {
-result *= a;
---b;
+result *= a1;
+--b1;
 }
 System.out.println("Answer = " + result);
 }
