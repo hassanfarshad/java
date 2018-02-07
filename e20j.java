b919e2bb316e9bb1b716e75c21c235d20b64bd08
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-02-07 16:43:53 +0530

    Create e20j.java

---
+++ e20j.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
+class e20
+{
+public static void main(String args[])
+{  
+int i,b;  
+Scanner d1=new Scanner(System.in);
+System.out.println("Enter the values");
+int a=d1.nextInt();
+ for(i=0;i<=a;i++)
+{
+b=a*i;
+System.out.println("OUTPUT is"+b);
+}
+}  
+}  
