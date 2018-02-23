0263fdee94db8e66a77a6d2479c7d303fde745be
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-02-23 16:56:44 +0530

    Create e46.java

---
+++ e46.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Scanner;
+class e46
+{
+	public static void main(String args[])
+	{
+		Scanner m=new Scanner(System.in);
+		System.out.println("Enter the number   :");
+		int a=m.nextInt();
+		int b=a+1;
+			System.out.println(b);
+	}
+}
