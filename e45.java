983443b8b890669edb0a0e1129f527d4894d11c9
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-02-23 16:53:38 +0530

    Create e45.java

---
+++ e45.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+class e40
+{
+	public static void main(String args[])
+	{
+		Scanner m=new Scanner(System.in);
+		System.out.println("Enter the number   :");
+		int a=m.nextInt();
+		String c=String.valueOf(a);
+		int d=c.length();
+			System.out.println(d);
+	}
+}
