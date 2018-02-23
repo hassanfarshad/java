ab8df318a732c17048c69826724a0b84ce6777ff
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-02-23 16:34:03 +0530

    Create e44.java

---
+++ e44.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Scanner;
+class e44
+{
+	public static void main(String args[])
+	{
+		Scanner m=new Scanner(System.in);
+		System.out.println("Enter the number   :");
+		int a=m.nextInt();
+		if(a>0&&a<=10)
+		{
+			System.out.println("YES");
+		}
+		else
+		{
+			System.out.println("NO");
+		}
+	}
+}
