c2c61a7126afa03b901a1f6152c767a9beb6a727
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-02-23 15:56:00 +0530

    Create e41.java

---
+++ e41.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
+class e41
+{
+	public static void main(String args[])
+	{
+		int i;
+		Scanner m=new Scanner(System.in);
+		System.out.println("Enter the String   :");
+		String a=m.nextLine();
+		int b=m.nextInt();
+		for(i=1;i<=b;i++)
+		{
+			System.out.println(a);
+		}
+	}
+}
