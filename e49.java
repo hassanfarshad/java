bc8d7992aebe52c3f5b1767eafc3bcdc1e71bffb
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-02-26 16:43:50 +0530

    Create e49.java

---
+++ e49.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+class e49
+{
+	public static void main(String[] args)
+	{
+		Scanner k=new Scanner(System.in);
+		System.out.println("Enter the values:");
+		int a=k.nextInt();
+		System.out.println("Enter the values:");
+		int b=k.nextInt();
+		int c=a+b;
+		System.out.println("SUM is :"+c);
+	}
+}
