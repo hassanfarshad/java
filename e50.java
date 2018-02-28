06d45f304bee9f665c81cd3beb3e1cca5e7fbf81
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-02-28 15:38:59 +0530

    Update e50.java

--- e50.java
+++ e50.java
@@ -1,27 +1,27 @@
 import java.util.*;
 import java.lang.*;
 import java.util.ArrayList;
-public class e50
+class e50
 {
      public static void main(String aa[])
      {
-     int a,b,f=0;
-     Scanner s=new Scanner(System.in);
-     a=s.nextInt();
+     int a,b,c=0;
+     Scanner D=new Scanner(System.in);
+     a=D.nextInt();
      while(a!=1)
      {
          if(a%2!=0)
          {
-             f=1;
+             c=1;
          }
          a=a/2;
         }
-     if(f==1){
+     if(c==1){
          System.out.println("No");
      }
  else
  {
          System.out.println("Yes");
  }
-    }
+}
 }
