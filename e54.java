f62f665291a9680db51ff329b9d135d5b7417744
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-02-28 16:57:04 +0530

    Create e54.java

---
+++ e54.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+class e52
+{
+     public static void main(String args[])
+     {
+     	int c=0;
+     	Scanner s=new Scanner(System.in);
+     	System.out.println("Enter the values");
+     	int a=s.nextInt();
+     	if(a%2==0)
+     	{
+     		System.out.println(a);
+     	}
+     	else
+     	{
+     		c=a-1;
+     		System.out.println(c);
+     		
+     	}
+     }
+}
