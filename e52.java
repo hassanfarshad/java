a343978047b0258b3b6c6b639a97735dd7252218
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-02-28 16:31:26 +0530

    Create e52.java

---
+++ e52.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+class e52
+{
+     public static void main(String args[])
+     {
+     Scanner s=new Scanner(System.in);
+     int b=s.nextInt();
+     switch(b)
+     {
+     	case 1:
+     		System.out.println("ONE");
+     		break;
+     	case 2:
+     		System.out.println("TWO");
+     		break;
+     	case 3:
+     		System.out.println("THREE");
+     		break;
+     	case 4:
+     		System.out.println("FOUR");
+     		break;
+     	case 5:
+     		System.out.println("FIVE");
+     		break;
+     	case 6:
+     		System.out.println("SIX");
+     		break;
+     	case 7:
+     		System.out.println("SEVEN");
+     		break;
+     	case 8:
+     		System.out.println("EIGHT");
+     		break;
+     	case 9:
+     		System.out.println("NINE");
+     		break;
+     	case 10:
+     		System.out.println("TEN");
+     		break;
+     	default:
+     		System.out.println("only 1-10");
+     		break;
+     	}
+     }
+}
