7f6236b19cb5994f7e0d72301c08d11d97cfe9f7
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-02 16:52:51 +0530

    Create e60.java

---
+++ e60.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+class e60
+{
+	public static void main(String args[])
+	{ 
+		int i,b=1,c=1,e;
+		Scanner d=new Scanner(System.in);
+		int a=d.nextInt();
+		for(i=0;i<=a;i++)
+		{
+		System.out.println(b);
+		e=b+c;
+		b=c;
+		c=e;
+		}
+	}
+}
