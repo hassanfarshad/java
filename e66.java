f7225abbcad3f9c212532fa18757f4504bb10d17
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-07 15:45:47 +0530

    Create e66.java

---
+++ e66.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+class e67
+{
+     public static void main(String aa[])
+     {
+     Scanner s=new Scanner(System.in);
+     int a=s.nextInt();
+     int b=(a/10)*10+10;
+     System.out.println(b);
+     }
+}
