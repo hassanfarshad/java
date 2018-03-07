577e760f60dcd2dd9b2428e96a5dff2dcf380df8
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-07 15:56:08 +0530

    Create e68.java

---
+++ e68.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class e68
+{
+     public static void main(String aa[])
+     {
+     Scanner s=new Scanner(System.in);
+     int a=s.nextInt();
+     if(a%7==0)
+     {
+     	System.out.println("YES");
+     }
+     else
+     {
+            System.out.println("NO");
+     }
+     }
+}
