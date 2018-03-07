b63bfcf427bbea7e19d6de3bda89f9314be1c663
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-07 16:04:53 +0530

    Create e70.java

---
+++ e70.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class e70
+{
+     public static void main(String args[])
+     {
+     Scanner s=new Scanner(System.in);
+     int a=s.nextInt();
+     int b=a*2;
+     	System.out.println(b);
+     }
+}
