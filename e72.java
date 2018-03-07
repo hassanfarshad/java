729cd6ffcd4f8ee69cecd07a5a63319d615b7598
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-07 16:59:12 +0530

    Create e72.java

---
+++ e72.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class e70
+{
+     public static void main(String args[])
+     {
+   Scanner s=new Scanner(System.in);
+     String a=s.nextLine();
+     String b[]=a.toCharArray();
+     for(int i=0;i<a.length();i++)
+     {
+     if(b[i])='a')||(a[i]='e')||(a[i]='i')||(a[i]='o')||(a[i]='u')||(a[i]='A')||(a[i]='E')||(a[i]='I')||(a[i]='O')||a.[i]='U'))
+     {
+     System.out.println("YES");
+     }
+     else
+     {
+     System.out.println("NO");	
+     }
+     }
+     }
+    }
+}
