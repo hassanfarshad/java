1bc13696d553a2095c4c95fe39caa158d870242a
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-07 16:01:23 +0530

    Create e69.java

---
+++ e69.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class e69
+{
+     public static void main(String aa[])
+     {
+     Scanner s=new Scanner(System.in);
+     int a=s.nextInt();
+     int b=s.nextInt();
+     int c=a-b;
+     if(c%2==0)
+     {
+     	System.out.println("EVEN");
+     }
+     else
+     {
+            System.out.println("ODD");
+     }
+     }
+}
