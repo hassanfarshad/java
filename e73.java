d63b99b0e0e802d65e7a873001da80a46cda42c3
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-09 15:58:52 +0530

    Create e73.java

---
+++ e73.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class e73
+{
+     public static void main(String args[])
+     {
+     Scanner s=new Scanner(System.in);
+     int a=s.nextInt();
+     int b=s.nextInt();
+     int c=s.nextInt();
+     if(a>b&&a<c) 
+     { 
+     System.out.println("YES");
+     }
+     else
+     {
+     System.out.println("NO");	
+     }
+     }
+}
