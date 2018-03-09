c2a28de825ea31fa2b86bc5f7c0c51f789f1fb3e
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-09 16:43:26 +0530

    Create e77.java

---
+++ e77.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class e77
+{
+  public static void main(String args[])
+  {
+  int f=1;
+  Scanner d=new Scanner(System.in);
+  int s=d.nextInt();
+  while(s!=0)
+  {
+   f=f*s;
+   s--;
+  }
+  System.out.println(f);
+  }
+ }
