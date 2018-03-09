296dcc0f3236154f4d67ad78f9b8cca7f0044aa5
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-09 16:13:59 +0530

    Create e74.java

---
+++ e74.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class e74
+{
+     public static void main(String args[])
+     {
+     Scanner s=new Scanner(System.in);
+     float a=s.nextFloat();
+     int c=Math.round(a);
+     System.out.println(c);	
+     }
+}
