2b7761e2d149ab59f9eba7fddf7b8368e56128f9
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-09 16:49:01 +0530

    Create e78.java

---
+++ e78.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class e78 
+{
+  public static void main(String args[])
+  {
+  int f=1;
+  Scanner d=new Scanner(System.in);
+  int s=d.nextInt();
+  int c=s%13;
+  if(c==0)
+  {
+  System.out.println("YES");
+  }
+  else
+  {
+  System.out.println("NO");
+  }
+  }
+ }
