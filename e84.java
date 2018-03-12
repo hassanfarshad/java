e45232627e85a36ff66c90894c516b65d9090ee8
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-12 16:57:54 +0530

    Create e84.java

---
+++ e84.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+class e84
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		char a=s.next().charAt(0);
+		int c = (int)a;
+		System.out.println(c);
+	}
+}
