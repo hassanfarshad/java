d16210f338a7766aab1ffd8e406eef5c73c65d19
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-12 16:26:40 +0530

    Create e81.java

---
+++ e81.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Scanner;
+class e81
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=b-a;
+		System.out.println(c);
+	}
+}
