1934be62fa0d85aca0e3b236a13c2fda3923b916
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-12 16:33:33 +0530

    Create e82.java

---
+++ e82.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+class e82
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the length and breath of farm :");
+		float a=s.nextFloat();
+		float b=s.nextFloat();
+		float c=b*a;
+		System.out.println(c);
+	}
+}
