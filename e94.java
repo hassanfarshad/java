e75ef25daad68378d6a2a0c72e83fda5dab85394
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-14 16:53:02 +0530

    Create e94.java

---
+++ e94.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class e94
+{
+     public static void main(String args[])
+     {
+     	int d=1;
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int[] c=new int[100];
+        for(int i=0;i<a;i++)
+        {
+            c[i]=s.nextInt();
+            d=c[b-1];
+        }
+        System.out.println(d);
+    }
+}
