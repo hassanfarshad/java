9809cc56c8081787956302ddd592f1fadb1a26a0
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-14 16:57:14 +0530

    Create e95.java

---
+++ e95.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class e95
+{
+     public static void main(String args[])
+     {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int c=s.nextInt();
+        int d=a*b*c;
+        int e=d/100;
+        System.out.println(e);
+    }
+}
