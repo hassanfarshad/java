518b06e89bf176c810155c856f69ad0189dfae10
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-14 16:43:26 +0530

    Create e93.java

---
+++ e93.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class e93
+{
+     public static void main(String args[])
+     {
+     	int c=0;
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the celsius :");
+        int a=s.nextInt();
+        int k=273+a;
+        System.out.println("Kelvin is: "+k);
+    }
+}
