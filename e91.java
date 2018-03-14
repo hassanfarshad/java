eee6beb1a44ced7ec661b681a48997b135fecd07
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-14 16:31:04 +0530

    Create e91.java

---
+++ e91.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class e91
+{
+     public static void main(String args[])
+     {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int c=s.nextInt();
+        int d=a*b*c;
+        System.out.println(d);
+    }
+}
