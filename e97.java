e5198f775d3c468b98b07e0e9b6aec6a0f2a5d6e
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-16 15:47:38 +0530

    Create e97.java

---
+++ e97.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+class e97
+{
+  public static void main(String args[])
+  {
+  Scanner d=new Scanner(System.in);
+  String s=d.next();
+  String b = new StringBuffer(s).reverse().toString();
+  System.out.println(b);
+  }
+ }
