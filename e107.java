4b4698587b87427b4e78b54e90545d48c728757d
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-16 16:49:10 +0530

    Create e107.java

---
+++ e107.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+class e107
+{
+    public static void main(String aa[])
+    {
+    Scanner s=new Scanner(System.in);
+    int a=s.nextInt();
+    int b=s.nextInt();
+    int c=s.nextInt();
+    int d=a*b;
+    int e=d/c;
+    System.out.println(e);
+   }
+}
