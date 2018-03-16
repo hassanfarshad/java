ae14f0968bc2ada745bd9a11e8e5242f467c7170
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-16 16:54:33 +0530

    Create e110.java

---
+++ e110.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+class e110
+{
+    public static void main(String aa[])
+    {
+    	int s=0;
+    Scanner d=new Scanner(System.in);
+    int a=d.nextInt();
+    for(int i=0;i<=a;i++)
+    {
+    	s=s+i;
+    }
+    System.out.println(s);
+   }
+}
