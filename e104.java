81436e638b35bc783870ecd1bc2b1a24ac505e67
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-16 16:43:37 +0530

    Create e104.java

---
+++ e104.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+class e104
+{
+    public static void main(String aa[])
+    {
+    Scanner s=new Scanner(System.in);
+    double a=s.nextDouble();
+    double b=s.nextDouble();
+    double c=Math.pow(a,b);
+    System.out.println(c);
+   }
+}
