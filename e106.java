e8138975a3025ee2f25724ebe4a3cd4be0a5b24b
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-19 16:53:38 +0530

    Create e106.java

---
+++ e106.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class e106 
+{
+  public static void main(String[] args)
+  {
+    Scanner p=new Scanner(System.in);
+    int b=p.nextInt();
+    int c=96+b;
+    char d=(char)c;
+    System.out.println(d);
+  }
+}
