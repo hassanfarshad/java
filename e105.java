8d5e1291012481605b09caa5a996a3806562f9df
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-19 16:53:06 +0530

    Create e105.java

---
+++ e105.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class alp 
+{
+  public static void main(String[] args)
+  {
+    Scanner a=new Scanner(System.in);
+    int b=a.nextInt();
+    int c=96+b;
+    char d=(char)c;
+    System.out.println(d);
+  }
+}
