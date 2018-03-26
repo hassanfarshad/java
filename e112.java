b9c3e0e191efb9ce9d9908446a03c79ec9e98966
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-26 16:22:35 +0530

    Create e112.java

---
+++ e112.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+class e112
+{
+    public static void main(String aa[]){
+   
+    Scanner s=new Scanner(System.in);
+    int a=s.nextInt();
+    int k=s.nextInt();
+    int b[]=new int[a];
+    for(int i=0;i<b.length;i++)
+    {
+        b[i]=s.nextInt();
+    }
+    for(int i=0;i<b.length;i++)
+    {
+        if(b[i]==k)
+        {
+            System.out.println("Yes");
+        }
+        else 
+        {
+        	System.out.println("NO");
+        }
+    }
+           
+   }
+}
