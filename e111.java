5594d34e9e7c178c2748fe72137e734a04d810ba
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-26 16:17:33 +0530

    Create e111.java

---
+++ e111.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class e111
+{
+      public static void main(String args[])
+      {
+      Scanner s=new Scanner(System.in);
+      int a=s.nextInt();
+      String b=String.valueOf(a);
+      int c=b.length();
+      int count=0;
+      for(int i=0;i<c;i++)
+      {
+          count++;
+      }
+       System.out.println(count);
+      }
+}
