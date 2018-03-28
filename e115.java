c5be03ef92848b9b73c01f2d19dc195801e95f45
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-03-28 15:57:43 +0530

    Create e115.java

---
+++ e115.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class e115
+{
+	public static void main (String[] args)
+	{
+	Scanner s=new Scanner(System.in);
+            int a=s.nextInt();
+            int[] b=new int[a];
+            int c=s.nextInt();
+            for(int i=0;i<a;i++)
+            {
+            b[i]=s.nextInt();	
+            }
+            System.out.println(b[c]);
+	}
+}
